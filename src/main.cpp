30d01ac752068d43dfaf2af2df8a38ef2f20dc9f
quocan0303 <quocan3399@gmail.com>
2023-10-09 12:47:45 +0700

    Remove PS4/ESPNOW protocal

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,12 @@
-// #include <Adafruit_MPU6050.h>
-#include <Arduino.h>
-#include "../lib/ps4_control/ps4_control.h"
-#include <PS4Controller.h>
-// #include "../lib/esp_wifi/init_esp_wifi.h"
 
+#include <Arduino.h>
 void setup(){
   // Start serial with platform baudrate/monitor speed
   Serial.begin(115200);
-  // pinMode(13, OUTPUT);
-  // digitalWrite(13, HIGH);
-  init_ps4();
-  // init_esp();
-  PS4.begin();
-  Serial.println("Ready.");
+
 }
 
 void loop() {
-  checkInput();
+  
 }
 
